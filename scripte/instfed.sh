759f84ecaf518d1e024cf6d00ceab06838c4714b
Kevin Luedemann <kevin27luedemann@gmail.com>
2016-01-16 15:11:42 +0100

    notwendige programme erweitert

--- scripte/instfed.sh
+++ scripte/instfed.sh
@@ -1,7 +1,7 @@
 #zuerst updates installieren
 dnf update
 #erste handvoll wichtige programme
-dnf install -y okular vim filezilla git ncdu cowsay fortune-firefly 
+dnf install -y okular vim filezilla git ncdu cowsay fortune-firefly mc feh ImageMagick arandr gcc gcc-c++
 #vlc mediaplayer installieren
 ./getnonfreerepo.sh
 dnf install -y vlc
@@ -9,5 +9,7 @@ dnf install -y vlc
 ./installgooglechrome.sh
 #install python
 ./instpython.sh
+#install i3blocks fuer Statusleiste
+./insti3blocks.sh
 #install texlive optional
 #dnf install texlive-scheme-full
---
+++ scripte/insti3blocks.sh
@@ -0,0 +1,5 @@
+git clone git://github.com/vivien/i3blocks
+cd i3blocks
+make clean debug
+make install
+cp i3blocks.config ~/.config/i3/
