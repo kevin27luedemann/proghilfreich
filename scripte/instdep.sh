3bd9e3bea38376f6269386c3827bf5cca15591be
kevin Luedemann <kevin27luedemann@googlemail.com>
2016-03-06 20:38:53 +0100

    instdep hinzu fuer Debain

---
+++ scripte/instdep.sh
@@ -0,0 +1,17 @@
+#zuerst updates installieren
+apt-get update
+#erste handvoll wichtige programme
+apt-get install -y okular filezilla ncdu cowsay fortune mc feh ImageMagick arandr gcc xterm ranger hexedit
+#vim mit latex installieren
+#dnf install -y vim vim-enhanced vim-latex vim-X11 texlive texlive-bibtex-bin texlive-latex-bin
+#vlc mediaplayer installieren
+#./getnonfreerepo.sh
+#dnf install -y vlc
+#google chrome installieren
+#./installgooglechrome.sh
+#install python
+#./instpython.sh
+#install i3blocks fuer Statusleiste
+#./insti3blocks.sh
+#install texlive optional
+#dnf install texlive-scheme-full
